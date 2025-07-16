f1e89cf242da7486b0d6002afdfbfa4054771464
sebastianf <sebastian_fohringer@icloud.com>
2025-07-16 19:54:44 +0200

    basic nvim settings

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- [[ Basic Settings ]]
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = false
+
+vim.o.number = true
+
+vim.o.mouse = 'a'
+
+-- [[ Custom Keymaps ]]
+
+vim.keymap.set('i', 'jj', '<Esc>')
