db5d635dd3a30ce6c78e0efee04410240bf49857
sebastianf <sebastian_fohringer@icloud.com>
2025-07-17 16:04:02 +0200

    which key + cleanup

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- recom. for nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.g.have_nerd_font = true
 
 vim.o.number = true
@@ -11,6 +15,13 @@ vim.o.mouse = 'a'
 
 vim.o.showmode = false
 
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.confirm = true
+
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
@@ -18,6 +29,17 @@ end)
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
 
 -- [[ Custom Keymaps ]]
 
@@ -54,4 +76,5 @@ require('lazy').setup({
 	require 'plugins.telescope',
 	require 'plugins.LSP',
 	require 'plugins.autocomplete',
+	require 'plugins.whichkey',
 })
